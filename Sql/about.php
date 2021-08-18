0017d60f46b3678151bcc68fe0b54beb36341155
Purva26 <83334958+Purva26@users.noreply.github.com>
2021-08-18 12:58:21 +0530

    Add files via upload

--- Sql/about.php
+++ Sql/about.php
@@ -1,1 +1,54 @@
 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@800&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <link rel="stylesheet" type="text/css" href="css/name.css">
+
+    <style type="text/css">
+    body{
+	   background-image:url("../image/about.jpg") ;
+	   position: center;
+     background-size:cover;
+	   height:100vh;
+    }
+    </style>
+  </head>
+
+  <body>
+    <?php
+      include 'name.php';
+    ?>
+    <br>
+    <br>
+    <h3> <strong class="cur">ABOUT US</strong></h3>
+    <p>
+      Online Banking System is connected to a network, that offers basic banking functions<br>
+      to users (access to bank account and make a Transaction, see Transaction History). 
+      <br>
+      This system is seamlessly integrated with Core Banking System.
+      <br>
+      Online Banking System is a complete web based with multi tier architecture and centralized <br>
+      database and banking solution covering all the functions of a bank. 
+      <br>
+      It supports multi currency transactions and all types of delivery channels.
+    </p>
+    <br>
+    <br>
+
+    <h3> <strong class="cur">CONTACT US</h3>
+    <hr color="brown" >
+    <h4>Creator:Purva Sutar</h4></strong>
+    <hr color="brown" >
+    <br>
+    <b><h5>Social Media</h5>
+      Insta:-</b> @purvaaa_08<br>
+      <b>Linkedin:-</b>purva-sutar-223928218<br>
+      <b>Email:-</b>purvasutar17@gmail.com
+  </body>
+</html>
